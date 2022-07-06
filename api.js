a338a6ac85d8b26463fb440907aac4c2fbf0b790
Sergio Moran <sergioMoran@gmail.com>
2022-07-06 17:01:20 -0600

    Agregado de las tutas

--- api.js
+++ api.js
@@ -1,5 +1,23 @@
 const ROUTE = "http://localhost:3015/tasks";
 
+export const deleteTask = async (id) => {
+  await fetch(`${ROUTE}/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export const saveTask = async (newTask) => {
+  const response = await fetch(`${ROUTE}/addTask`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTask),
+  });
+  return await response.json();
+};
+
 export const getTasks = async () => {
   const response = await fetch(ROUTE, {
     method: "GET",
